934b34de41a3840abc5d5afc1c2ccb10f93a7d7c
NADEE-MJ <nadeem.maida@gmail.com>
2024-04-03 19:16:35 -0700

    added folders and updated files

---
+++ scripts/quiz-complete.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const score = urlParams.get("score");
+    const total = urlParams.get("total");
+
+    document.getElementById("score").textContent = score;
+    document.getElementById("total").textContent = total;
+  });
