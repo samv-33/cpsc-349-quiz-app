2a0b3e5f3e2a9c6e2f57f4e5b966ce6a800042b8
NADEE-MJ <nadeem.maida@gmail.com>
2024-04-05 18:58:50 -0700

    updated readme.md and fixed issues with quiz skip button

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # cpsc-349-quiz-app
 
-browser-sync start --server --browser "firefox" --files "stylesheets/*.css, *.html"
+## Description
+
+This is a quiz app that we created for our CPSC 349 class. It is a simple quiz app, users select from 4 quiz options. The quiz is timed and the user is given a score at the end of the quiz. The user can also view the correct answers to the quiz. The app tracks your previous quiz attempts in local storage and allows you to view them in the quiz history page. The app is built using HTML, CSS, and JavaScript. We used the Bootstrap framework to style the app. We also used the browser-sync package to run the app locally on our computers.
+
+## Installation
+
+To run this app, you will need to have Node.js installed on your computer. You can download it [here](https://nodejs.org/en/). Once you have Node.js installed, you can run the following command to install the necessary dependencies:
+
+```bash
+npm install -g browser-sync
+
+browser-sync start --server --browser "firefox" --files "css/*.css, *.html"
+```
+
+## Authors
+
+Nadeem Maida
+Sam Vo
+Karson Lant
--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,7 +1,9 @@
 (function () {
   let userScore = 0;
+  let totalQuestions = 0;
   let quizResults = [];
-  let totalQuestions = 0; // Variable to store total number of questions
+  let currentQuestionIndex = 0;
+  let quizData;
 
   // Function to get query parameter for quiz number
   function getQueryParam(param) {
@@ -22,10 +24,7 @@
     }
   }
 
-  let currentQuestionIndex = 0;
-  let quizData;
-
-  async function displayQuestion() {
+  function displayQuestion() {
     if (!quizData || quizData.length <= currentQuestionIndex) {
       console.log("No more questions or quiz data not loaded.");
       return;
@@ -44,9 +43,7 @@
     quizData[currentQuestionIndex].options.forEach((option) => {
       const button = document.createElement("button");
       button.setAttribute("type", "button");
-      button.classList.add("btn", "btn-primary", "m-2");
-      button.style.padding = "3rem 10rem"; // Set padding directly
-      button.style.fontSize = "1.5rem"; // Set font size directly
+      button.classList.add("btn", "btn-primary", "m-2", "quiz-button");
       button.textContent = option;
       button.onclick = function () {
         selectOption(option);
@@ -55,69 +52,77 @@
     });
   }
 
-  function selectOption(option) {
-    const question = quizData[currentQuestionIndex];
-    if (option === question.answer) {
-      console.log("Correct!");
-      userScore++; // Increment score if the answer is correct
-    } else {
-      console.log("Wrong!");
-    }
+  function redirectToQuizComplete() {
+    let allQuizResults = localStorage.getItem("allQuizResults");
+    allQuizResults = allQuizResults ? JSON.parse(allQuizResults) : [];
+    allQuizResults.push({
+      quiz: getQueryParam("quiz"),
+      questionsAndAnswers: quizResults,
+      score: userScore,
+      totalQuestions: totalQuestions,
+      timestamp: new Date().toISOString(),
+    });
 
+    // Save quiz results, user score, and total questions to local storage
+    localStorage.setItem("allQuizResults", JSON.stringify(allQuizResults));
+    localStorage.setItem("recentQuizResults", JSON.stringify(quizResults));
+    localStorage.setItem("recentUserScore", userScore);
+    localStorage.setItem("recentTotalQuestions", totalQuestions);
+
+    window.location.href = `quiz-complete.html`;
+  }
+
+  function moveToNextQuestion(question, option, correct) {
     quizResults.push({
       question: question.question,
       selectedOption: option,
       correctOption: question.answer,
-      correct: option === question.answer,
+      correct: correct,
     });
-
-    console.log("Quiz results:", quizResults); // Log the quiz results
 
     if (currentQuestionIndex < quizData.length - 1) {
       currentQuestionIndex++;
       displayQuestion();
     } else {
       console.log("Quiz finished");
-      let allQuizResults = localStorage.getItem("allQuizResults");
-      allQuizResults = allQuizResults ? JSON.parse(allQuizResults) : [];
-      allQuizResults.push({
-        quiz: getQueryParam("quiz"),
-        questionsAndAnswers: quizResults,
-        score: userScore,
-        totalQuestions: totalQuestions,
-        timestamp: new Date().toISOString(),
+
+      redirectToQuizComplete();
+    }
+  }
+
+  function selectOption(option) {
+    const question = quizData[currentQuestionIndex];
+    correct = false;
+    if (option === question.answer) {
+      console.log("Correct!");
+      userScore++; // Increment score if the answer is correct
+      correct = true;
+    } else {
+      console.log("Wrong!");
+    }
+
+    moveToNextQuestion(question, option, correct);
+  }
+
+  function addSkipButtonHandler() {
+    const skipButton = document.getElementById("skipButton");
+    skipButton.addEventListener("click", function () {
+      const question = quizData[currentQuestionIndex];
+
+      moveToNextQuestion(question, null, false);
+    });
+  }
+
+  function main() {
+    addSkipButtonHandler();
+    const quizName = getQueryParam("quiz");
+    if (quizName) {
+      loadQuizData(quizName).then((data) => {
+        quizData = data.questions;
+        totalQuestions = quizData.length; // Set total number of questions
+        displayQuestion(); // Display the first question
       });
-
-      // Save quiz results, user score, and total questions to local storage
-      localStorage.setItem("recentQuizResults", JSON.stringify(quizResults));
-      localStorage.setItem("allQuizResults", JSON.stringify(allQuizResults));
-      localStorage.setItem("recentUserScore", userScore);
-      localStorage.setItem("recentTotalQuestions", totalQuestions);
-      // Redirect to quiz-complete.html with score and total questions as query parameters
-      window.location.href = `quiz-complete.html`;
     }
   }
-
-  document.getElementById("skipButton").addEventListener("click", function () {
-    if (currentQuestionIndex < quizData.length - 1) {
-      currentQuestionIndex++;
-      displayQuestion();
-    } else {
-      console.log("Quiz finished");
-      // Save quiz results, user score, and total questions to local storage
-      localStorage.setItem("quizResults", JSON.stringify(quizResults));
-      localStorage.setItem("userScore", userScore);
-      localStorage.setItem("totalQuestions", totalQuestions);
-      window.location.href = "quiz-complete.html";
-    }
-  });
-
-  const quizName = getQueryParam("quiz");
-  if (quizName) {
-    loadQuizData(quizName).then((data) => {
-      quizData = data.questions;
-      totalQuestions = quizData.length; // Set total number of questions
-      displayQuestion(); // Display the first question
-    });
-  }
+  main();
 })();
